3f39d30b45075b3e28e89974819b484e39306040
Andre LaFleur <andre.lafleur@utah.edu>
2017-02-09 17:55:14 -0700

    Setup NoteCommand class

--- src/main/java/org/cincospenguinos/note_taker/NoteCommand.java
+++ src/main/java/org/cincospenguinos/note_taker/NoteCommand.java
@@ -3,6 +3,7 @@ package org.cincospenguinos.note_taker;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 import java.util.logging.Logger;
 
@@ -10,36 +11,59 @@ import java.util.logging.Logger;
  * NoteCommand - manages notes for each player
  */
 public class NoteCommand implements CommandExecutor {
+
+    private enum NoteCommandType {
+        LIST, DELETE, CREATE, READ, INVALID
+    }
+
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
-        if(strings.length == 0 || !verifyCommand(strings))
-            return false;
+        if(commandSender instanceof Player){
+            NoteCommandType t = getCommandType(strings);
 
-        // TODO: Figure out what command is requested
+            if(t == NoteCommandType.INVALID)
+                return false;
 
-        for(String str : strings){
-            Logger.getLogger("minecraft").info("[NoteCommand] " + str);
-        }
+            switch(t){
+                case CREATE:
+                    return create(strings);
+                case LIST:
+                    return list();
+                case DELETE:
+                    return delete(strings);
+                case READ:
+                    return read(strings);
+            }
+        } else
+            commandSender.sendMessage("This command is only permitted for players, not server admins.");
 
         return true;
     }
 
-    private boolean verifyCommand(String[] commandStrings) {
+    /**
+     * Returns which type of command the user is requesting.
+     *
+     * @param commandStrings - the command strings that came in from the user
+     * @return Some command type
+     */
+    private NoteCommandType getCommandType(String[] commandStrings){
+        if(commandStrings.length == 0)
+            return NoteCommandType.INVALID;
+
         String cmd = commandStrings[0];
 
         if(cmd.equalsIgnoreCase("list"))
-            return true;
-        else if(cmd.equalsIgnoreCase("delete")){
-            return commandStrings.length == 2 && isValidInteger(commandStrings[1]);
-        } else if(cmd.equalsIgnoreCase("create")){
+            return NoteCommandType.LIST;
+        else if(cmd.equalsIgnoreCase("delete") && commandStrings.length == 2 && isValidInteger(commandStrings[1]))
+            return NoteCommandType.DELETE;
+        else if(cmd.equalsIgnoreCase("create") && commandStrings.length > 1)
+            return NoteCommandType.CREATE;
+        else if(cmd.equalsIgnoreCase("read") && commandStrings.length == 2 && isValidInteger(commandStrings[1]))
+            return NoteCommandType.READ;
 
-        } else if(cmd.equalsIgnoreCase("read")){
-
-        }
-
-        return true;
+        return NoteCommandType.INVALID;
     }
 
-    private boolean create(){
+    private boolean create(String[] command){
         // TODO: this
         return true;
     }
@@ -49,11 +73,21 @@ public class NoteCommand implements CommandExecutor {
         return true;
     }
 
-    private boolean delete(){
+    private boolean delete(String[] command){
         // TODO: This
         return true;
     }
 
+    private boolean read(String[] command){
+        // TODO: This
+        return true;
+    }
+
+    /**
+     * Checks to see if the String passed is a valid integer.
+     * @param s - string to check
+     * @return true if it's a valid string
+     */
     private boolean isValidInteger(String s){
         try {
             Integer.parseInt(s);
